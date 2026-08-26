a1b841fec96561650978905274e96e965c3b6c82
guilherme <guiguchampi@gmail.com>
2026-08-26 09:13:35 -0300

    criamos estados

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 class personagem {
     constructor(nome, vida, ataque){
-        this.name = nome,
-        this.vida = vida,
-        this.ataque = ataque
+        this.name = nome;
+        this.vida = vida;
+        this.ataque = ataque;
     }
 
     causaDano(personagem){
@@ -13,30 +13,26 @@ recebeDano(quantidade){
     this.vida = this.vida - quantidade
 
     if(this.vida <=0){
-        this.vida = "elemento"
+        this.vida = "eliminado"
     }
 }
 
+estaVivo(){
+    return this.vida > 0
+}
+
+
+}
+
+
 const druida = new personagem("kode",80, 25)
-
 const guerreiro = new personagem("thorin",60, 20)
-
 const mago = new personagem("gandalfe",60, 35)
-
 const arqueiro = new personagem("legolas",80, 25)
 
-const jogador = new personagem("neymar",60, 30)
+druida.causadano(mago)
+druida.causadano(mago)
 
-const trabalhador = new personagem("zeze",90, 60)
+console.log(mago.estaVivo())
 
-const zelador = new personagem("bem10",50, 20)
 
-guerreiro.causaDano(jogador)
-druida.causaDano(trabalhador)
-mago.causaDano(zelador)
-arqueiro.causaDano(guerreiro)
-jogador.causaDano(druida)
-trabalhador.causaDano(mago)
-zelador.causaDano(arqueiro)
-
-console.log(guerreiro.vida, druida.vida, mago.vida, arqueiro.vida, jogador.vida, trabalhador.vida, zelador.vida)
